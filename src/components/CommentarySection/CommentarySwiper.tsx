9f9cc3306feaa2a53bc8d292d407b1625f68ce9d
JerryWski <jarek_wasilewski@yahoo.com>
2024-02-22 06:00:48 +0100

    adding commentary section

---
+++ src/components/CommentarySection/CommentarySwiper.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+import '../../utils/Swiper.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { clients } from '../../utils/commentaryList';
+
+const CommentarySwiper = () => {
+  return (
+    <Swiper
+      autoplay={{
+        delay: 3500,
+        disableOnInteraction: false,
+      }}
+      breakpoints={{
+        900: {
+          width: 1200,
+          slidesPerView: 2,
+        },
+      }}
+      slidesPerView={1}
+      spaceBetween={10}
+      centeredSlides={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination, Autoplay]}
+      className='mySwiper'
+    >
+      {clients.map((client) => (
+        <SwiperSlide key={client.id}>
+          <div className='swiper-container'>
+            <div className='img-wrapper'>
+              <img src={client.image} alt='man-photo' />
+            </div>
+            <h2 className='swiper-header'>{client.header}</h2>
+            {/* <h3 className='swiper-subheader'>{client.subheader}</h3> */}
+            <p className='swiper-text'>{client.text}</p>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default CommentarySwiper;
