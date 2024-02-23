3603751634ad50d1c73fb98166d42270876e5590
JerryWski <jarek_wasilewski@yahoo.com>
2024-02-23 07:25:09 +0100

    adding footer

---
+++ src/components/FooterSection/FooterSection.tsx
@@ -0,0 +1,29 @@
+import './FooterSection.css'
+
+const FooterSection: React.FC = () => {
+  const currentYear = (): number => {
+    return new Date().getFullYear();
+  };
+  return (
+    <>
+        <span className='footer_overline'></span>
+      <div className='footer'>
+        <p className='footer_year'>
+        ©{currentYear()} 
+          <a
+            className='footer_link'
+            href='https://jerrywski.github.io/Portfolio-Site/'
+          >
+            Jerry_Wski.
+          </a>
+          &nbsp; Wszelkie prawa zastrzeżone
+          <a href="">
+          </a>
+        </p>
+        <a className='privacy' href="">Polityka Prywatności</a>
+      </div>
+    </>
+  );
+};
+
+export default FooterSection;
